f8582a15bb528414978b8abd6a8c378a5eec180b
Erandi Amarasekara <126141215+Imalshi1031@users.noreply.github.com>
2024-11-24 18:34:39 +0530

    chore:minor changes

--- src/app/imp/page.tsx
+++ src/app/imp/page.tsx
@@ -1,31 +1,42 @@
 import DashboardCard from '@/app/imp/_components/dashboard';
 import { Folder, MessageCircle, Newspaper, User } from 'lucide-react';
 
+
+const cardData = [
+  {
+    title: 'Requests',
+    count: 10,
+    icon: <Newspaper className='text-slate-500' size={72} />,
+  },
+  {
+    title: 'Quotation Sent',
+    count: 12,
+    icon: <Folder className='text-slate-500' size={72} />,
+  },
+  {
+    title: 'Accepted Quotations',
+    count: 7,
+    icon: <User className='text-slate-500' size={72} />,
+  },
+  {
+    title: 'Comments',
+    count: 120,
+    icon: <MessageCircle className='text-slate-500' size={72} />,
+  },
+];
+
+
 export default function Home() {
+  
+    
     return (
       <>
+        
         <div className='flex flex-col md:flex-row justify-between gap-5 mb-5'>
-          <DashboardCard
-            title='Requests'
-            count={10}
-            icon={<Newspaper className='text-slate-500' size={72} />}
-          />
-          <DashboardCard
-            title='Quotation Sent'
-            count={12}
-            icon={<Folder className='text-slate-500' size={72} />}
-          />
-          <DashboardCard
-            title='Accepted Quotations'
-            count={7}
-            icon={<User className='text-slate-500' size={72} />}
-          />
-          <DashboardCard
-            title='Comments'
-            count={120}
-            icon={<MessageCircle className='text-slate-500' size={72} />}
-          />
+          {cardData.map((card, index) => (<DashboardCard key={index} {...card} />))}
         </div>
+           
+        
 
         {/* <AnalyticsChart />
         <PostsTable title='Latest Posts' limit={5} /> */}
